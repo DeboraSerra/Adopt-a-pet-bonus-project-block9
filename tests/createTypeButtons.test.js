4042b951ba87c736d420e14b42cbf3773becc748
Débora Serra <debora.r.serra@gmail.com>
2022-02-12 14:45:13 -0300

    Traduz o teste createTypeButtons

--- tests/createTypeButtons.test.js
+++ tests/createTypeButtons.test.js
@@ -9,13 +9,13 @@ document.body.innerHTML = text;
 require('../mocks/fetchSimulator');
 const { createTypeButtons } = require('../script2');
 
-describe('Testa a função createTypeButtons', () => {
-  it('Testa se animalTypes é uma array de tipos', async () => {
+describe('Test the function createTypeButtons', () => {
+  it('tests if animalTypes is an array of the types', async () => {
     const expected = ['Dog', 'Cat', 'Rabbit', 'Small & Furry', 'Horse', 'Bird', 'Scales, Fins & Other', 'Barnyard'];
     const { animalTypes } = await createTypeButtons();
     expect(animalTypes).toEqual(expected);
   });
-  it('Testa se ao chamar a função createTypeButtons os botões são criados', () => {
+  it('tests if when the function createTypeButtons is called the buttons are created', () => {
     const expected = ['Todos os tipos', 'Todos os tipos', 'Dog', 'Cat', 'Rabbit', 'Small & Furry', 'Horse', 'Bird', 'Scales, Fins & Other', 'Barnyard'];
     const buttons = document.getElementsByClassName('btn-dark');
     const buttonsText = Array.from(buttons)
