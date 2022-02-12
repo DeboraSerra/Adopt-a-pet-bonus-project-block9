5a01b6ad8837c69338081e1b3bf1940fa94af6fd
Débora Serra <debora.r.serra@gmail.com>
2022-02-12 14:42:00 -0300

    Traduz teste do createAnimalCards

--- tests/createAnimalsCards.test.js
+++ tests/createAnimalsCards.test.js
@@ -11,50 +11,50 @@ require('../mocks/fetchSimulator');
 const { getAllAnimals } = require('../script2');
 const { createAnimalsCards } = require('../script2');
 
-describe('Testa a função createAnimalsCards', () => {
-  it('Verifica se a função createAnimalsCards cria uma seção com o id do animal', async () => {
+describe('Test the function createAnimalsCards', () => {
+  it('Verifies if the function createAnimalsCards create a section with the id of the animal', async () => {
     const animals = await getAllAnimals();
     createAnimalsCards(animals);
     const section = document.getElementById(animals[0].id.toString());
     expect(section).toBeTruthy();
   });
-  it('Verifica se a seção criada com createAnimalsCards possui a classe \'card-body\'', async () => {
+  it('Verifies if the section created by createAnimalsCards have the class \'card-body\'', async () => {
     const animals = await getAllAnimals();
     createAnimalsCards(animals);
     const section = document.getElementById(animals[0].id.toString());
     expect(section.classList).toContain('card-body');
   });
-  it('Verifica se createAnimalsCards adiciona um elemento h2 à seção criada para o animal', async () => {
+  it('Verifies if createAnimalsCards adds an h2 element to the section created to the animal', async () => {
     const animals = await getAllAnimals();
     createAnimalsCards(animals);
     const h2 = document.querySelector('.card-body h2');
     expect(h2).toBeTruthy();
   });
-  it('Verifica se o elemento h2 criado tem o nome do animal', async () => {
+  it('Verifies if the h2 element created have the animal\'s name', async () => {
     const animals = await getAllAnimals();
     createAnimalsCards(animals);
     const h2 = document.querySelector('.card-body h2');
     expect(animals[0].name).toEqual(h2.innerText);
   });
-  it('Verifica createAnimalsCards adiciona um elemento img à seção criada para o animal', async () => {
+  it('Verifies if createAnimalsCards add an img element to the section created to the animal', async () => {
     const animals = await getAllAnimals();
     createAnimalsCards(animals);
     const img = document.querySelector('.card-body img');
     expect(img).toBeTruthy();
   });
-  it('Verifica se o elemento img criado tem um link para a imagem do animal', async () => {
+  it('Verifies if the img element have the link to the animal\'s image', async () => {
     const animals = await getAllAnimals();
     createAnimalsCards(animals);
     const img = document.querySelector('.card-body img');
     expect(img.src.includes('http')).toBe(true);
   });
-  it('Verifica createAnimalsCards adiciona um elemento \'p\' à seção criada para o animal', async () => {
+  it('Verifies if createAnimalsCards adds a \'p\' element to the animal\'s section', async () => {
     const animals = await getAllAnimals();
     createAnimalsCards(animals);
     const p = document.querySelector('.card-body p');
     expect(p).toBeTruthy();
   });
-  it('Verifica se o elemento \'p\' criado tem a descrição do animal', async () => {
+  it('Verifies if the \'p\' element created have the animals description', async () => {
     const animals = await getAllAnimals();
     createAnimalsCards(animals);
     const p = document.querySelector('.card-body p');
